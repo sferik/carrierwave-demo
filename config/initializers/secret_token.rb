e41093a76177e5970f8ac5f2a81897eab05a3eb8
Erik Michaels-Ober <sferik@gmail.com>
2013-05-08 10:43:05 -0700

    New Rails app to demonstrate file uploads with CarrierWave

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarrierwaveDemo::Application.config.secret_token = '751564027fe844b4bbe47e592d980b70491d1240cdde19a87bd5dc8f8d7dd518aa1e9ca64e4626dd3a98dcf1619862da49421a5be8f845af2ab5035a9e1fdf14'
