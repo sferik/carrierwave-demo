ce97c95b75da81b4e70a9e8f07a46ccc3359892d
Erik Michaels-Ober <sferik@gmail.com>
2013-05-08 11:25:17 -0700

    Add a small image size and display that on the index page

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -40,13 +40,17 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :scale => [100, 100]
+    process :resize_to_limit => [50, 50]
+  end
+
+  version :small do
+    process :resize_to_limit => [200, 200]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-    %w(jpg jpeg png)
+    %w(jpg jpeg gif png)
   end
 
   # Override the filename of the uploaded files:
